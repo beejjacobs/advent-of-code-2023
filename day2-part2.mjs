e66c2ce0230bda174c6c8e3258626fd837afd2e6
Ben Jacobs <ben.jacobs@vanti.co.uk>
2023-12-14 23:01:13 +0000

    feat: day2

---
+++ day2-part2.mjs
@@ -0,0 +1,60 @@
+import fs from 'fs/promises';
+
+async function run() {
+    const content = await fs.readFile('day2.txt');
+    const lines = content.toString()
+        .split('\n')
+        .map(l => l.trim())
+        .filter(Boolean);
+    console.log(lines.length);
+
+    const values = lines
+        .map(removeGameSuffix)
+        .map(powerOfSets);
+
+    const total = values.reduce((total, v) => total += v, 0);
+    console.log(total);
+}
+
+/**
+ * @typedef {Object} Info
+ * @property {number} id
+ * @property {string} line
+ */
+/**
+ *
+ * @param {string} line
+ * @returns {number}
+ */
+function powerOfSets(line) {
+    const sets = line.split(';');
+
+    const min = {
+        red: 0,
+        green: 0,
+        blue: 0
+    };
+
+    for (const set of sets) {
+        const parts = set.split(',').map(p => p.trim());
+        for (const part of parts) {
+            const [num, colour] = part.split(' ');
+            const qty = parseInt(num);
+            if (qty > min[colour]) {
+                min[colour] = qty;
+            }
+        }
+    }
+    return min.red * min.green * min.blue;
+}
+
+/**
+ * @param {string} line
+ * @returns {string}
+ */
+function removeGameSuffix(line) {
+    const colonIndex = line.indexOf(':');
+    return line.substring(colonIndex + 1);
+}
+
+run().catch(console.error);
---
+++ day2.mjs
@@ -0,0 +1,73 @@
+import fs from 'fs/promises';
+
+const config = {
+    red: 12,
+    green: 13,
+    blue: 14
+};
+
+async function run() {
+    const content = await fs.readFile('day2.txt');
+    const lines = content.toString()
+        .split('\n')
+        .map(l => l.trim())
+        .filter(Boolean);
+    console.log(lines.length);
+
+    const values = lines
+        .map(extractGamePrefix)
+        .filter(info => isGamePossible(info, config))
+        .map(info => info.id);
+
+    const total = values.reduce((total, v) => total += v, 0);
+    console.log(total);
+}
+
+/**
+ * @typedef {Object} Info
+ * @property {number} id
+ * @property {string} line
+ */
+
+/**
+ * @typedef {Object} Config
+ * @property {number} red
+ * @property {number} green
+ * @property {number} blue
+ */
+
+/**
+ *
+ * @param {Info} info
+ * @param {Config} config
+ * @returns {boolean}
+ */
+function isGamePossible({id, line}, config) {
+    const sets = line.split(';');
+
+    for (const set of sets) {
+        const parts = set.split(',').map(p => p.trim());
+        for (const part of parts) {
+            const [num, colour] = part.split(' ');
+            const qty = parseInt(num);
+            if (qty > config[colour]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+/**
+ * @param {string} line
+ * @returns {Info}
+ */
+function extractGamePrefix(line) {
+    const colonIndex = line.indexOf(':');
+    const number = line.substring(0, colonIndex).replace('Game ', '');
+    const id = parseInt(number);
+    const remaining = line.substring(colonIndex + 1);
+    return {id, line: remaining};
+}
+
+run().catch(console.error);
