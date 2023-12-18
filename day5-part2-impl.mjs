19613b7624ab2e8ffdce3bd100eb502aefe5f257
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2023-12-18 09:18:44 +0000

    dev: day5 wip

---
+++ day5-part2-impl.mjs
@@ -0,0 +1,196 @@
+/**
+ * @param {string} input 
+ * @returns {number}
+ */
+export function computeLowestLocation(input) {
+    const lines = input
+        .split('\n')
+        .map(l => l.trim());
+    console.log(lines.length);
+    
+    const info = parseLines(lines);
+    const locations = computeLocations(info);
+    const loc = Math.min(...locations);
+    console.log('Min Location', loc);
+    return loc;
+}
+
+/**
+ * @param {Info} info
+ * @return {number[]}
+ */
+function computeLocations(info) {
+    let ranges = info.seedRanges;
+    // go through each mapping for each [seed] range
+    for (const maps of info.maps) {
+        // console.log(ranges, maps);
+        const mappedRanges = [];
+        for (const range of ranges) { 
+            const rangesWithMappings = [];
+            for (const {sourceRange, inc} of maps) {
+                const inter = rangeIntersection(sourceRange, range);
+                if (inter) {
+                    // map to new range with +inc
+                    mappedRanges.push([inter[0] + inc, inter[1] + inc]);
+                    rangesWithMappings.push(inter);
+                }
+            }
+            // now we have all ranges with mappings, compute the inverse ranges that have no mappings
+            mappedRanges.push(...inverseRanges(range, rangesWithMappings));
+        }
+        ranges = mappedRanges;
+    }
+    // console.log(ranges);
+    return ranges.map(r => r[0]); // start of each range
+}
+
+/**
+ * @typedef {Object} RangeMap
+ * @property {number[]} sourceRange
+ * @property {number} inc
+ */
+
+/**
+ * @typedef {Object} Info
+ * @property {number[][]} seedRanges 
+ * @property {RangeMap[][]} maps
+ */
+
+
+/**
+ * @param {string[]} lines
+ * @returns {Info}
+ */
+function parseLines(lines) {
+    const seedValues = lines.shift()
+        .replace('seeds: ', '')
+        .split(' ')
+        .map(n => parseInt(n.trim()));
+
+    const seedRanges = [];
+    for (let i = 0; i < seedValues.length; i+=2) {
+        const start = seedValues[i];
+        const length = seedValues[i + 1];
+        seedRanges.push([start, start + length]);
+    }
+
+    const rangeMaps = [];
+
+    let id = null;
+    let maps = [];
+
+    for (const line of lines) {
+        if (line === '') {
+            if (id) {
+                rangeMaps.push(maps);
+                id = null;
+                maps = [];
+            }
+        } else if (line.includes('-to-')) {
+            id = line.split(' ')[0];
+        } else {
+            const [dest, source, range] = line
+                .split(' ')
+                .map(num => parseInt(num));
+            maps.push({
+                sourceRange: [source, source + range],
+                inc: dest - source
+            });
+        }
+    }
+    if (id) {
+        rangeMaps.push(maps);
+    }
+
+    return {seedRanges, maps: rangeMaps};
+}
+
+/**
+ * Returns the range that intersects r1 and r2
+ *
+ * @param {number[]} r1
+ * @param {number[]} r2
+ * @returns {number[]|null}
+ */
+export function rangeIntersection(r1, r2) {
+    const [r1Start, r1End] = r1;
+    const [r2Start, r2End] = r2;
+    // start in range, end out of range
+    // r1 |-----------------|
+    // r2          |--------------|
+    if (inRange(r1, r2Start) && !inRange(r1, r2End)) {
+        return [r2Start, r1End];
+    }
+    // start out of range, end in range
+    // r1          |--------------|
+    // r2 |-----------------|
+    if (!inRange(r1, r2Start) && inRange(r1, r2End)) {
+        return [r1Start, r2End];
+    }
+    // r2 within r1
+    // r1 |--------------|
+    // r2   |----------|
+    if (inRange(r1, r2Start) && inRange(r1, r2End)) {
+        return r2;
+    }
+    // r1 within r2
+    // r1  |----------|
+    // r2 |--------------| 
+    if (inRange(r2, r1Start) && inRange(r2, r1End)) {
+        return r1;
+    }
+    return null;
+}
+
+/**
+ * 
+ * @param {number[]} range 
+ * @param {number[][]} rangesToRemove 
+ * @returns {number[][]} 
+ */
+export function inverseRanges(range, rangesToRemove) {
+    if (rangesToRemove.length === 0) {
+        return [range];
+    }
+    const inverse = [];
+    let previous;
+    for (let i = 0; i < rangesToRemove.length; i++) {
+        const [start, end] = rangesToRemove[i];
+        if (i === 0) { // first one
+            if (start !== range[0]) {
+                // |----------------------------|
+                //           |--------------|
+                // xxxxxxxxxx
+                inverse.push([range[0], start - 1]);
+            }
+        } else {
+            if (start !== (previous[1] + 1)) {
+                // |----------------------------|
+                //    |----|
+                //              |-----|
+                //          xxxx
+                inverse.push([previous[1] + 1, start - 1]);
+            }
+        }
+        if (i === rangesToRemove.length - 1) {
+            if (end !== range[1]) {
+                // |----------------------------|
+                //    |----|
+                //              |-----|
+                //                     xxxxxxxxxx
+                inverse.push([end + 1, range[1]]);
+            }
+        } 
+        previous = [start, end];
+    }
+    return inverse;
+}
+
+/**
+ * @param {number[]} range
+ * @param {number} num
+ * @returns {boolean}
+ */
+export function inRange([start, end], num) {
+    return num <= end && num >= start;
+}
---
+++ day5-part2-impl.test.mjs
@@ -0,0 +1,140 @@
+import {describe, test} from 'node:test';
+import { computeLowestLocation, inverseRanges, rangeIntersection } from './day5-part2-impl.mjs';
+import assert from 'node:assert/strict';
+
+describe('computeLowestLocation', () => {
+    test('test input', () => {
+        const input = `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`;
+        const lowest = computeLowestLocation(input);
+        assert.equal(lowest, 46);
+    });
+
+});
+
+describe('rangeIntersection', () => {
+    const tests = [
+        {
+            name: 'start in range',
+            r1: [1, 10],
+            r2: [5, 20],
+            want: [5, 10]
+        },
+        {
+            name: 'end in range',
+            r1: [10, 20],
+            r2: [5, 15],
+            want: [10, 15]
+        },
+        {
+            name: 'within r1',
+            r1: [10, 20],
+            r2: [12, 14],
+            want: [12, 14]
+        },
+        {
+            name: 'within r2',
+            r1: [10, 20],
+            r2: [8, 25],
+            want: [10, 20]
+        },
+        {
+            name: 'no intersection',
+            r1: [10, 20],
+            r2: [30, 40],
+            want: null
+        },
+        {
+            name: 'test case 1',
+            r1: [25, 95],
+            r2: [81, 95],
+            want: [81, 95]
+        }
+    ];
+
+    for (const t of tests) {
+        test(t.name, () => {
+            const got = rangeIntersection(t.r1, t.r2);
+            assert.deepEqual(got, t.want);
+        });
+    }
+});
+
+describe('inverseRanges', () => {
+    const tests = [
+        {
+            name: 'nothing to remove',
+            range: [1, 10],
+            rangesToRemove: [],
+            want: [[1, 10]]
+        },
+        {
+            name: 'everything to remove',
+            range: [1, 10],
+            rangesToRemove: [[1, 10]],
+            want: []
+        },
+        {
+            name: 'from start',
+            range: [1, 10],
+            rangesToRemove: [[1, 5]],
+            want: [[6, 10]]
+        },
+        {
+            name: 'to end',
+            range: [1, 10],
+            rangesToRemove: [[6, 10]],
+            want: [[1, 5]]
+        },
+        {
+            name: 'multiple',
+            range: [1, 10],
+            rangesToRemove: [[2, 3], [5, 6], [7, 8]],
+            want: [[1, 1], [4, 4], [9, 10]]
+        },
+        {
+            name: 'multiple',
+            range: [1, 10],
+            rangesToRemove: [[1, 1], [5, 6], [7, 8]],
+            want: [[2, 4], [9, 10]]
+        }
+    ];
+
+    for (const t of tests) {
+        test(t.name, () => {
+            const got = inverseRanges(t.range, t.rangesToRemove);
+            assert.deepEqual(got, t.want);
+        });
+    }
+});
